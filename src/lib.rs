5e33d61590c60f73d12ad8fd9ab1d94ecc01a53e
Antoine Poinsot <darosior@protonmail.com>
2020-08-12 15:28:04 +0200

    script: add helpers to generate outputs and redeem script descriptors
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 mod revault_error;
+mod scripts;
 mod transations;
+
+pub use scripts::{get_default_unvault_descriptors, get_default_vault_descriptors};
+pub use transations::{RevaultPrevout, RevaultTransaction, RevaultTxOut};
