29897f0dfc15dc018e85228e94243d26fcbaa578
Antoine Poinsot <darosior@protonmail.com>
2020-08-17 16:25:08 +0200

    transations: test transations against libbitcoinconsensus
    This also corrects our sighash computation, as it turns out we were
    using the legacy (pre-bip143) one..
    
    We need to run against rust-bitcoin's master but rust-miniscript's
    traits implementations would get mad if we do, so i had to use hacky
    branch until rust-bitcoin 0.24 is released.
    
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/revault_error.rs
+++ src/revault_error.rs
@@ -4,7 +4,9 @@ use std::{error, fmt};
 pub enum RevaultError {
     TransactionCreation(String),
     ScriptCreation(String),
+    Signature(String),
     InputSatisfaction(String),
+    TransactionVerification(String),
 }
 
 impl fmt::Display for RevaultError {
@@ -16,8 +18,14 @@ impl fmt::Display for RevaultError {
             RevaultError::ScriptCreation(ref e) => {
                 write!(f, "Revault script creation error: {}", e)
             }
+            RevaultError::Signature(ref e) => {
+                write!(f, "Revault transaction signature error: {}", e)
+            }
             RevaultError::InputSatisfaction(ref e) => {
                 write!(f, "Revault input satisfaction error: {}", e)
+            }
+            RevaultError::TransactionVerification(ref e) => {
+                write!(f, "Revault transaction verification error: {}", e)
             }
         }
     }
