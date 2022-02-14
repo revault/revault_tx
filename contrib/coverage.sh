9e531acaa0a804239ddc91da15c76f5185632d97
Antoine Poinsot <darosior@protonmail.com>
2022-02-14 13:47:12 +0100

    contrib: fixup the source-based coverage script

--- contrib/coverage.sh
+++ contrib/coverage.sh
@@ -1,15 +1,26 @@
+# Generate a source-based code coverage from the unit tests, then an HTML report using
+# grcov, and start it with Firefox.
+# https://rustc-dev-guide.rust-lang.org/llvm-coverage-instrumentation.html
+# https://github.com/mozilla/grcov
+
 set -ex
 
 if ! command -v grcov &>/dev/null; then
-    cargo install grcov
+    cargo +nightly install grcov
 fi
+
+rustup +nightly component add llvm-tools-preview
+
+mkdir -p ./target/grcov/
 
 cargo clean
 
-LLVM_PROFILE_FILE="revault_tx_coverage_%m.profraw" RUSTFLAGS="-Zinstrument-coverage" RUSTDOCFLAGS="$RUSTFLAGS -Z unstable-options --persist-doctests target/debug/doctestbins" cargo +nightly build --all-features
-LLVM_PROFILE_FILE="revault_tx_coverage_%m.profraw" RUSTFLAGS="-Zinstrument-coverage" RUSTDOCFLAGS="$RUSTFLAGS -Z unstable-options --persist-doctests target/debug/doctestbins" cargo +nightly test --all-features
+RUSTUP_TOOLCHAIN=nightly LLVM_PROFILE_FILE="$PWD/target/grcov/revault_tx_coverage_%m.profraw" RUSTFLAGS="-Zinstrument-coverage" RUSTDOCFLAGS="$RUSTFLAGS -Z unstable-options --persist-doctests target/debug/doctestbins" cargo +nightly build --all-features
+RUSTUP_TOOLCHAIN=nightly LLVM_PROFILE_FILE="$PWD/target/grcov/revault_tx_coverage_%m.profraw" cargo +nightly test --all-features
 
-grcov . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --llvm -o ./target/grcov/
+RUSTUP_TOOLCHAIN=nightly grcov ./target/grcov/ --source-dir ./ --binary-path ./target/debug/ -t html --branch --ignore-not-existing --llvm -o ./target/grcov/
 firefox target/grcov/index.html
+
+rm ./target/grcov/*.profraw
 
 set +ex
